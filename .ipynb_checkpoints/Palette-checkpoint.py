f17aacb464ecf0ecd026b94079bf49ce545418c0
Shailesh <shailesh@Shaileshs-MacBook-Air.local>
2025-02-23 11:46:41 +0100

    suhs

--- .ipynb_checkpoints/Palette-checkpoint.py
+++ .ipynb_checkpoints/Palette-checkpoint.py
@@ -40,7 +40,7 @@ def changeface(face_image):
             # Ensure that the face doesn't go beyond the palette boundaries
             if left + face_width <= palette_width and top + face_height <= palette_height:
                 # Paste the new face with transparency using the image as its own mask
-                palette_image.paste(new_face_image_resized, (left, top), new_face_image_resized)
+                palette_image.paste(new_face_image, (left, top), new_face_image)
     
     # Save the updated palette image into a BytesIO object to avoid saving it to disk
     img_bytes = BytesIO()
